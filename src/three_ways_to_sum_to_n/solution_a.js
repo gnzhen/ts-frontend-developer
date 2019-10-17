80f3fba26360dc8179d3a548d9f714523937f565
gnzhen <yi.zhen.choy@adv-fusionex.com>
2019-10-17 22:44:13 +0800

    submit question 1

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -3,6 +3,12 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+var sum_to_n = function (n) {
+    var sum = 0;
+    for (var i = 0 + 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(sum_to_n(5));
