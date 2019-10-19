51cf169b05fa6ac07d76ee3ca6788b1c5a302198
gnzhen <mioyazhen1996@gmail.com>
2019-10-19 23:32:01 +0800

    done ui

---
+++ src/fancy_form/script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    $(".input-text").focus(function () {
+        var inputLine = $(this).parent(".input-line");
+        var label = inputLine.prev(".input-label");
+        inputLine.addClass("highlight");
+        label.addClass("shrink-label");
+    });
+    $(".input-text").focusout(function () {
+        var inputLine = $(this).parent(".input-line");
+        var label = inputLine.prev(".input-label");
+        if (inputLine.find("input").val())
+            return;
+        inputLine.removeClass("highlight");
+        label.removeClass("shrink-label");
+    });
+
+    var pwIconOpen = "M16,12.8c3,0,5.1,1.9,6,2.9c-0.9,1.2-3,3.4-6,3.4c-2.8,0-5-2.2-6-3.4C11,14.7,13.1,12.8,16,12.8z M16,11.6c-4.8,0-7.6,4.1-7.6,4.1s3.1,4.7,7.6,4.7c4.9,0,7.6-4.7,7.6-4.7S20.9,11.6,16,11.6z M16,13.5c-1.4,0-2.5,1.2-2.5,2.5c0,1.4,1.1,2.5,2.5,2.5c1.4,0,2.5-1.1,2.5-2.5C18.5,14.6,17.4,13.4,16,13.5z"
+    var pwIconClose = "M20.8,10.1l-2.1,2c-0.9-0.3-1.8-0.4-2.7-0.4c-4.8,0-7.6,4.2-7.6,4.2c0.9,1.3,2,2.4,3.3,3.3l-1.8,1.8l0.9,0.9l11-10.8L20.8,10.1z M17,13.7c-1.3-0.6-2.8,0-3.3,1.3c-0.3,0.6-0.3,1.4,0,2l-1.1,1.1c-1-0.6-1.8-1.4-2.6-2.3c0.9-1,3-2.9,6-2.9c0.6,0,1.1,0.1,1.7,0.2L17,13.7z M15.2,18.4l3.2-3.2c0.4,1.3-0.3,2.8-1.6,3.2C16.3,18.6,15.7,18.6,15.2,18.4z M23.6,15.8c0,0-2.7,4.7-7.6,4.7c-0.8,0-1.7-0.2-2.4-0.5l1-1c0.5,0.1,0.9,0.2,1.4,0.2c3,0,5.1-2.2,6-3.4c-0.7-0.7-1.4-1.4-2.3-1.9l0.9-0.9C21.8,13.7,22.8,14.7,23.6,15.8L23.6,15.8z";
+    $(".icon-password").find("path").attr("d", pwIconOpen);
+    $(".icon-password").click(function () {
+        var icon = $(this).find("path");
+        var password = $(this).parent().prev(".input-text");
+        if (icon.attr("d") == pwIconOpen) {
+            icon.attr("d", pwIconClose);
+            password.attr("type", "text");
+        }
+        else {
+            icon.attr("d", pwIconOpen);
+            password.attr("type", "password");
+        }
+    });
+
+}
